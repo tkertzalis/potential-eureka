01a71deb86ea05ea8a6a823ed56a4e9c143d543c
tkertzalis <57686873+tkertzalis@users.noreply.github.com>
2020-12-08 16:54:57 +0200

    aa
    diorthosi

--- js/myscore4.js
+++ js/myscore4.js
@@ -8,7 +8,7 @@ function draw_empty_board() {
 	var t ='<table id="score4_table">';
 	for(var i=1; i<8; i++){
 		t += '<tr>';
-		for var(j=1; j<7; j++){
+		for (var j=1; j<7; j++){
 			t += '<td class="score4_square" id="square_'+j+'_'+i+'">' + j + ',' + i + '</td>';
 		}
 		t += '</tr>';
